8b3ba57404c78918d0fb1570e8c69012c0696385
tuanle290901 <tuanle290901@gmail.com>
2023-09-08 10:53:38 +0700

    commit

--- js/login.js
+++ js/login.js
@@ -14,7 +14,7 @@ function login() {
                 if (r.name === "ROLE_ADMIN") {
                     console.log(data.token);
                     location.href = "index.html";
-                } else if (r.name === "ROLE_User") {
+                } else if (r.name === "ROLE_USER") {
                     location.href = "index.html";
                 }
                 localStorage.setItem("token", data.token);
