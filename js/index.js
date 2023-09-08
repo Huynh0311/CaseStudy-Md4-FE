5fbd2721646bcb68168638fa453520495cbb004c
basonzzzz1 <132861064+basonzzzz1@users.noreply.github.com>
2023-09-08 16:17:38 +0700

    truoc pull 16:17 08/09

--- js/index.js
+++ js/index.js
@@ -42,6 +42,24 @@ function show(arr) {
     }
     document.getElementById("body-content").innerHTML = str;
 }
+function search(){
+    let search = document.getElementById("form1").value;
+    console.log(search);
+    $.ajax({
+        type: "GET",
+        headers: {
+            'Accept': 'application/json',
+            "Authorization": "Bearer " + token
+        },
+        url: "http://localhost:8080/api/products/search/"+ search,
+        success: function (data) {
+            show(data);
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    });
+}
 function showProduct(idP){
     window.location = "product_detail.html?id=" + idP;
 }
